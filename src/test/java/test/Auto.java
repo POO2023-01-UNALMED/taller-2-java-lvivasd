f4e3ebd0d36bb1b1b32e7be58d122be049c49ab2
lvivasd <lvivasd@unal.edu.co>
2023-02-20 14:20:08 -0500

    Intento 6

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -11,9 +11,6 @@ public class Auto {
 	public int cantidadAsientos() {
 		int cantidad_asientos = 0;
 		for (int i = 0; i < asientos.length; i++) {
-			if (asientos[i] == null) {
-				continue;
-			}
 			if (asientos[i] instanceof Asiento) {
 				cantidad_asientos += 1;
 			}
@@ -21,15 +18,15 @@ public class Auto {
 		return cantidad_asientos;
 	}
 	public String verificarIntegridad() {
-		boolean integridad = true;
 		for (int i = 0; i < asientos.length; i++) {
-			if (this.registro != asientos[i].registro) {
-				integridad = false;
+			if (asientos[i] == null) {
+				continue;
+			}
+			else if (this.registro != asientos[i].registro) {
 				return("Las piezas no son originales");
 			}
 		}
 		if (this.registro != motor.registro) {
-			integridad = false;
 			return("Las piezas no son originales");
 		}
 		else {
